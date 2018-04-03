ca469cc109913f0b83fe8ec3ab133666f210cabc
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-04-03 17:54:02 +0000

    Adds looping exercises

--- go-outside.js
+++ go-outside.js
@@ -1,4 +1,4 @@
-var temperature = 12;
+/*var temperature = 12;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -9,3 +9,20 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+Replace the comment inside the function whichSchool such that the
+ function returns the string Elementary School if age is below 13,
+  Secondary School if age is between 13 and 18 (both inclusive),
+   and Lighthouse Labs in all other cases.
+*/
+
+
+var vacationDays = 2 + 3;
+
+if (vacationDays <= 5) {
+  console.log("Not enough for a long vacation.");
+} else if (vacationDays === 5) {
+  console.log("We can take a whole week off, not bad.");
+} else {
+  console.log("We're gonna have a great time!");
+}
